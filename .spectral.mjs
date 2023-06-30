8906e9ae100fa2efd3ccb5da698580cb5a95d36d
Adenipekun Blessing <blessingadenipekun@gmail.com>
2023-06-30 11:39:43 +0100

    Added 4 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIwODUxOA/spectral.js?branch_slug=main&token=7a9d8d6d-96c6-4930-871b-16ba7c41523d";
+export default { extends: ruleset };
